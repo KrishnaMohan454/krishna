6fdf31bc5f95607819cb3a184b3e6262024a8e91
Hanna Mac 2 <HannaIndia@Hannas-Mac-mini-2.local>
2017-03-16 10:22:04 +0530

    mohan

---
+++ modelClass.h
@@ -0,0 +1,13 @@
+//
+//  modelClass.h
+//  454G
+//
+//  Created by Hanna Mac 2 on 16/03/17.
+//  Copyright © 2017 Hanna Mac 2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface modelClass : NSObject
+
+@end
---
+++ modelClass.m
@@ -0,0 +1,13 @@
+//
+//  modelClass.m
+//  454G
+//
+//  Created by Hanna Mac 2 on 16/03/17.
+//  Copyright © 2017 Hanna Mac 2. All rights reserved.
+//
+
+#import "modelClass.h"
+
+@implementation modelClass
+
+@end
